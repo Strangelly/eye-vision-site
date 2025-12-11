8adc7e65f8f401888c22b94f09d4f75d205dc6c2
Strangelly <inanahmed10@gmail.com>
2025-12-11 12:18:23 +0600

    cart

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -1,3 +1,4 @@
+from store.models import Product
 
 class Cart():
     def __init__(self, request):
@@ -10,3 +11,20 @@ class Cart():
             cart = self.session['session_key'] = {}
 
         self.cart = cart
+
+    def add(self, product, quantity=1):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+        self.cart[product_id]['quantity'] += quantity
+        self.session.modified = True
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+    
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        return products
+    
+
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
 
 def cart_summary(request):
-    return render(request, "c_summary.html", {})
+    cart = Cart(request)
+    cart_items = cart.get_prods() 
+    return render(request, "c_summary.html", {"cart_items": cart_items})
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+    if request.method == "POST":
+        product_id = request.POST.get("product_id")
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+
+        cart_quantity = cart.__len__()
+        
+        return JsonResponse({"cart_quantity": len(cart)})
+    
+    
 
 def cart_delete(request):
     pass
