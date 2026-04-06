19b795e19bb2c8e28e2eb8e8238c0171d28e9652
Strangelly <inanahmed10@gmail.com>
2026-04-06 21:22:19 +0600

    deploy2

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': 'roundhouse.proxy.rlwy.net',
         'PORT': '34749',
     }
